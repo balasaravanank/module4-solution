cebe2a2234b76488ba6e9134b69adb2c38c9c8dc
Bala Saravanan K <130923584+balasaravanank@users.noreply.github.com>
2026-01-12 08:33:08 +0530

    Create SpeakHello.js

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+(function (window) {
+
+  var speakWord = "Hello";
+
+  var speakHello = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.speakHello = speakHello;
+
+})(window);
